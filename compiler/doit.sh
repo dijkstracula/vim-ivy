d3ef902d4b3791a8a192033e8ec8c2ce748b2aa3
Nathan Taylor <ntaylor@cs.utexas.edu>
2022-01-01 16:38:35 -0700

    Do all compilation in a tempdir

--- compiler/doit.sh
+++ compiler/doit.sh
@@ -4,26 +4,33 @@
 # all .ivy files need to be in the current working directory, and there isn't a
 # way of parsing a file without either calling into the checker or extraction
 # to C++.  So, this ensures we change into the directory that the file is
-# contained within, and, we stomp over $PATH so g++ is a no-op now.
+# contained within, and, we stomp over $PATH so g++ is a no-op now.  To avoid
+# stomping over any files in the user's Ivy file's directory, everything happens
+# inside a tempdir.
 
 filepath=$1
 
-SOURCE=$(dirname ${BASH_SOURCE[0]})
+# Add this script's directory to $PATH so when ivyc calls g++ it calls our
+# no-op script.
+SOURCE=$(realpath $(dirname ${BASH_SOURCE[0]}))
+export PATH="$SOURCE:$PATH"
 
-olddir=$(PWD)
-newdir=$(dirname $filepath)
+olddir=$(dirname $filepath)
 fname=$(basename $filepath)
 
+newdir=$(mktemp -d /tmp/${fname}.XXXXXX || exit 1)
+cp ${olddir}/*.ivy $newdir
 cd $newdir
 
-export PATH="$SOURCE:$PATH"
+# Note the gnarly hack in our use of sed: because Ivy will only report the
+# filename and not the full path (and because we copied it to a tempdir
+# anyway), we have to resubstitute the original absolute file in its place so
+# QuickFix can find it.  Unfortunately, this will very likely cause weirdness
+# if the error is in another file (i.e. module instantiation), so, the right
+# solution is for Ivy to give us a target=parse option so Vim can call ivyc
+# directly.
 ivyc target=test $fname \
     | grep ': error: '  \
     | sed -e "s:^${fname}:${filepath}:g"
 
-# Note the gnarly hack in our use of sed: because Ivy will only report the
-# filename and not the path, we have to resubstitute the original absolute file 
-# in its place so QuickFix can find it.  Unfortunately, this will very likely
-# cause weirdness if the error is in another file, so, the right solution is
-# for Ivy to either give us the absolute path or let us specify files not 
-# in the current working directory...
+rm -r $newdir
